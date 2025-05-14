d6dccf2af2128e793325de079e7977ba8e1e3f24
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-14 13:28:11 -0600

    i did an example of ForEach

--- List/ContentView.swift
+++ List/ContentView.swift
@@ -7,18 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct User: Identifiable {
+    let id = UUID()  // Cada usuario tiene un identificador único
+    let name: String
+    let role: String
+}
+
+let users = [
+    User(name: "Max", role: "iOS Developer"),
+    User(name: "Sofía", role: "UX Designer"),
+    User(name: "Carlos", role: "Product Manager"),
+    User(name: "Pepe", role: "Boss")
+]
+
+struct HolaView: View{
+    var body: some View{
+        ForEach(users) { user in
+            VStack {
+                Text(user.name)
+                Text(user.role)
+            }
+            .padding()
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(10)
         }
-        .padding()
     }
 }
 
+
 #Preview {
-    ContentView()
+    HolaView()
 }

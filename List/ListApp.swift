36807279af34848794d68cdb052d17283bb51f64
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-14 13:07:28 -0600

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Benjamin Rojo on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

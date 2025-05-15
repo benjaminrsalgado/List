34067f0ab80fb4ac7eab32243bc6c9ed1aeb8672
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-15 15:07:16 -0600

    i add style to the two  dividers

--- List/File.swift
+++ List/File.swift
@@ -11,22 +11,21 @@ struct StyledDividerView: View {
     var body: some View {
         VStack(spacing: 20) {
             Text("Item 1")
-            Divider()
+            Rectangle()
+                .fill(LinearGradient(colors: [.red, .orange], startPoint: .leading, endPoint: .trailing))
                 .frame(height: 2)
-                .background(Color.red)
+                .shadow(color: .red.opacity(0.3), radius: 3, x: 0, y: 2)
             Text("Item 2")
-            Divider()
-                .frame(height: 20)
-                .background(Color.blue)
+            Rectangle()
+                .fill(LinearGradient(colors: [.blue, .purple], startPoint: .leading, endPoint: .trailing))
+                .frame(height: 2)
+                .shadow(color: .blue.opacity(0.3), radius: 3, x: 0, y: 2)
             Text("Item 3")
         }
         .padding()
     }
 }
 
-
-
 #Preview {
     StyledDividerView()
 }
-
